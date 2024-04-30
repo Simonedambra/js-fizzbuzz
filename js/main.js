b03251d71f8d002cdc8dcd4153d83132ae53681d
Simonedambra <simonedambra130102@gmail.com>
2024-04-30 15:53:32 +0200

    [upd] fizzbuzz

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 'use strict'
+for (let x = 0; x < 100; x++) {
+
+    if (x % 3 == 0 && x % 5 == 0) {
+        console.log("FizzBuzz");
+    }
+
+    else if (x % 3 == 0) {
+        console.log("fizz");
+    }
+
+    else if (x % 5 == 0) {
+        console.log("buzz");
+    }
+
+    else {
+        console.log(x);
+    }
+
+
+
+
+
+
+
+
+}
